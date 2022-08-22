119fd672bc53bcda8d405407c0202ec9ee5c2fb5
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-22 10:47:44 +0530

    Declared the 2 arrays with city names and distances from Zurich

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -5,6 +5,8 @@ package com.jap;
  */
 public class SortingCities {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Declare the 2 arrays with city names and distances from Zurich
+        String[] cityNames = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
+        int[] distanceFromZurich = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
     }
 }
