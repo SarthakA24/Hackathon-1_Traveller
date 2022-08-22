8ddaccd68209ca2b18efa2b8e988e14f43698d7d
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-22 12:24:20 +0530

    Added the Test case for the method convertToUpperCase() to check the output

--- src/test/java/com/jap/SortingCitiesTest.java
+++ src/test/java/com/jap/SortingCitiesTest.java
@@ -2,6 +2,7 @@ package com.jap;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class SortingCitiesTest {
     // Declare the object to call the method in SortingCities.java class
@@ -19,5 +20,11 @@ public class SortingCitiesTest {
         sortingCities = null;
     }
 
-    //write all the test cases here
+    // Write all the test cases here
+    // Test case for the method convertToUpperCase() to check the output
+    @Test
+    void convertToUpperCaseSuccess() {
+        String[] expectedOutput = {"BERN", "LUCERN", "INTERLAKEN", "MURREN"};
+        //Assertions.assertEquals();
+    }
 }
