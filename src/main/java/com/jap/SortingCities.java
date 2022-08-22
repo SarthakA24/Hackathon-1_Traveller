e226765075e9094a096287170dfa8ddb64c23fa8
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-22 10:44:45 +0530

    Reformatted the source code

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -2,12 +2,9 @@ package com.jap;
 
 /**
  * Hello world!
- *
  */
-public class SortingCities
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class SortingCities {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
