3601f6fc34963542caeb427bb94af4cc169c3ad0
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-22 11:39:35 +0530

    Refactored the loop variable in the method sortBasedOnDistance()

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -47,17 +47,17 @@ public class SortingCities {
         int tempDistance;
         String tempCityName;
         // Start the loop iterations te sort the array
-        for (int indexOne = 0; indexOne < sortedDistanceFromZurich.length; indexOne++) {
-            for (int indexTwo = 0; indexTwo < sortedDistanceFromZurich.length - indexOne - 1; indexTwo++) {
-                if (sortedDistanceFromZurich[indexTwo] > sortedDistanceFromZurich[indexTwo + 1]) {
+        for (int pass = 0; pass < sortedDistanceFromZurich.length; pass++) {
+            for (int index = 0; index < sortedDistanceFromZurich.length - pass - 1; index++) {
+                if (sortedDistanceFromZurich[index] > sortedDistanceFromZurich[index + 1]) {
                     // Swap the numbers in distance array
-                    tempDistance = sortedDistanceFromZurich[indexTwo];
-                    sortedDistanceFromZurich[indexTwo] = sortedDistanceFromZurich[indexTwo + 1];
-                    sortedDistanceFromZurich[indexTwo + 1] = tempDistance;
+                    tempDistance = sortedDistanceFromZurich[index];
+                    sortedDistanceFromZurich[index] = sortedDistanceFromZurich[index + 1];
+                    sortedDistanceFromZurich[index + 1] = tempDistance;
                     // Swap the values in city array
-                    tempCityName = sortedCityNames[indexTwo];
-                    sortedCityNames[indexTwo] = sortedCityNames[indexTwo + 1];
-                    sortedCityNames[indexTwo + 1] = tempCityName;
+                    tempCityName = sortedCityNames[index];
+                    sortedCityNames[index] = sortedCityNames[index + 1];
+                    sortedCityNames[index + 1] = tempCityName;
                 }
             }
         }
