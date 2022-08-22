108d13e2a2669f532cba3dda9db84011e80fbaf7
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-22 13:38:34 +0530

    Wrote the test case for the method sortBasedOnDistance() to check the sorting logic

--- src/test/java/com/jap/SortingCitiesTest.java
+++ src/test/java/com/jap/SortingCitiesTest.java
@@ -56,20 +56,26 @@ public class SortingCitiesTest {
     // Test case for the method cityGreaterThanOrEqualTo270KmFromZurich to find the city with distance of more than 270kms from Zurich
     @Test
     public void cityGreaterThanOrEqualTo270KmFromZurichSuccess() {
-        Assertions.assertEquals("Geneva", sortingCities.cityGreaterThanOrEqualTo270KmFromZurich(new String[]{"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"}, new int[]{138, 52, 118, 136, 85, 276, 103, 87, 214, 101}));
+        Assertions.assertEquals("Geneva", sortingCities.cityGreaterThanOrEqualTo270KmFromZurich(new String[]{"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"}, new int[]{138, 52, 118, 136, 85, 276, 103, 87, 214, 101}), "There is some error in the logic");
     }
 
     // Test case for the method cityGreaterThanOrEqualTo270KmFromZurich to return null for incorrect inputs
     @Test
     public void cityGreaterThanOrEqualTo270KmFromZurichFailure() {
-        Assertions.assertNull(sortingCities.cityGreaterThanOrEqualTo270KmFromZurich(null, new int[]{0}));
-        Assertions.assertNull(sortingCities.cityGreaterThanOrEqualTo270KmFromZurich(null, new int[]{}));
+        Assertions.assertNull(sortingCities.cityGreaterThanOrEqualTo270KmFromZurich(null, new int[]{0}), "There is some error in the logic");
+        Assertions.assertNull(sortingCities.cityGreaterThanOrEqualTo270KmFromZurich(null, new int[]{}), "There is some error in the logic");
         Assertions.assertNull(sortingCities.cityGreaterThanOrEqualTo270KmFromZurich(new String[]{"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"}, new int[]{}), "There is some error in logic");
     }
 
     // Test case for the method sortBasedOnDistance() to check the sorting logic
     @Test
     public void sortBasedOnDistanceSuccess() {
-
+        String[] cityNames = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
+        int[] distanceFromZurich = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
+        String[] expectedCityOutput = {"Lucerne", "Engelberg", "Basel", "Jungfraujoch", "Murren", "Interlaken", "Grindelwald", "Bern", "Zermatt", "Geneva"};
+        int[] expectedDistanceOutput = {52, 85, 87, 101, 103, 118, 136, 138, 214, 276};
+        sortingCities.sortBasedOnDistance(cityNames, distanceFromZurich);
+        Assertions.assertArrayEquals(expectedCityOutput, cityNames, "There is some error in the logic");
+        Assertions.assertArrayEquals(expectedDistanceOutput, distanceFromZurich, "There is some error in the logic");
     }
 }
